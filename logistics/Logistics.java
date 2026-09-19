33e33cd3e25186929861fa5311a2249774facc0a
bakdauletbegaliev <bagadaulet@bk.ru>
2026-09-19 22:19:03 +0500

    feat: implement Factory Method pattern (stage 1)

---
+++ logistics/Logistics.java
@@ -0,0 +1,13 @@
+package logistics;
+
+import transport.Transport;
+
+public abstract class Logistics {
+
+    public abstract Transport createTransport();
+
+    public void planDelivery(String cargo, String destination) {
+        Transport transport = createTransport();
+        transport.deliver(cargo, destination);
+    }
+}
---
+++ transport/Transport.java
@@ -0,0 +1,6 @@
+package transport;
+
+
+public interface Transport {
+    void deliver(String cargo, String destination);
+}
